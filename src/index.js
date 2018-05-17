b2d528e4275795a2da1bf6f43c722e42b55b7ad9
V Saravanan <saravanan.resume@gmail.com>
2018-05-17 23:35:40 +0800

    add friend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import PropTypes from 'prop-types'
 
-class HelloUser extends React.Component {
+class AddFriend extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newFriend: ''
+    }
+
+    this.updateNewFriend = this.updateNewFriend.bind(this)
+    this.handleAddNew = this.handleAddNew.bind(this)
+  }
+  updateNewFriend(e) {
+    this.setState({
+      newFriend: e.target.value
+    })
+  }
+  handleAddNew() {
+    this.props.addNew(this.state.newFriend)
+    this.setState({
+      newFriend: ''
+    })
+  }
   render() {
     return (
-      <div> Hello, {this.props.name}</div>
+      <div>
+        <input
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}> Add Friend </button>
+      </div>
     )
   }
 }
 
-ReactDOM.render(<HelloUser name="Tyler" />, document.getElementById('root'));
+// AddFriend.propTypes = {
+//   addNew: PropTypes.func.isRequired
+// }
+
+class ShowList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {this.props.names.map((friend) => {
+            return <li> {friend} </li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+// ShowList.defaultProps = {
+//   names: []
+// }
+
+class FriendsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Tyler McGinnis',
+      friends: [
+        'Jake Lingwall',
+        'Sarah Drasner',
+        'Merrick Christensen'
+      ],
+    }
+
+    this.addFriend = this.addFriend.bind(this)
+  }
+  addFriend(friend) {
+    this.setState((state) => ({
+      friends: state.friends.concat([friend])
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<FriendsContainer />, document.getElementById('root'));
