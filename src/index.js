e250526b4024fa5113e7e265489235fd850fb681
V Saravanan <saravanan.resume@gmail.com>
2018-05-17 23:14:10 +0800

    event

--- src/index.js
+++ src/index.js
@@ -8,14 +8,26 @@ class HelloUser extends React.Component {
     this.state = {
       username: 'tylermcginnis'
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e) {
+    this.setState({
+      username: e.target.value
+    })
   }
   render() {
     return (
       <div>
-        Hello {this.state.username}
+        Hello {this.state.username} <br />
+        Change Name:
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
       </div>
     )
   }
 }
-
 ReactDOM.render(<HelloUser />, document.getElementById('root'));
