634d918dd77715f60ebe7c2dd979fee087eedb01
V Saravanan <saravanan.resume@gmail.com>
2018-05-18 16:44:00 +0800

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
- 
-class MyComponent extends React.Component{
-        render(){
-        return(   
-              <div>
-               <h1>Hello</h1>
-               <Header name="maxx" id="101"/>
-           </div>
-        );
-    }
-}
- 
-function Header(props) {
-    return (              
-           <div>
-            <Footer name = {props.name} id = {props.id}/>
-           </div>
-    );
-}
-function Footer(props) {
-    return (             
-           <div>             
-            <h1> Welcome : {props.name}</h1>             
-            <h1> Id is : {props.id}</h1>
-           </div>
-  );
-}
-ReactDOM.render(
-   <MyComponent/>, document.getElementById('root')
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
 );
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+
+
+ReactDOM.render(<BasicExample/>, document.getElementById('root'))
