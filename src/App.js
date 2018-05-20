2ad9a194bfcdff31279412a8711e98a34828df92
Viswanathan Saravanan <saravanan.resume@gmail.com>
2018-05-20 22:30:35 +0800

    codesandbox
    tested in codesandbox

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-import axios from 'axios'
+import axios from "axios";
 
 class UserCompany extends React.Component {
-  state = {company: undefined, loaded: false}
-  fetchCompany  = () => {
+  state = { company: undefined, loaded: false };
+  fetchCompany = () => {
     axios({
-      url: 'https://api.github.com/graphql',
-      method: 'post',
-      data : {
-        query : `{
+      url: "https://api.github.com/graphql",
+      method: "post",
+      data: {
+        query: `{
           user(login: "${this.props.username}") {
             company
           }
         }`
       },
-      headers: {Authorization: `bearer 6cde3e34072e88d43e4e5f7cdda9f1c7ba36c7f1`},
-    })
-    .then(response => { this.setState({company: response.data.data.user.company, loaded: true}) },
-    error => { this.setState({error, loaded: true})},
-  )
-  }
-  
-  componentDidMount ()  {
-    this.fetchCompany()
-  }
-  
-  render() {
-    return (
-      this.state.company || 'Unknown' 
+      headers: {
+        Authorization: `bearer f39b8b14158d792feac63c9ec086c93a4ab8be76`
+      }
+    }).then(
+      response => {
+        this.setState({
+          company: response.data.data.user.company,
+          loaded: true
+        });
+      },
+      error => {
+        this.setState({ error, loaded: true });
+      }
+    );
+  };
 
-    )
+  componentDidMount() {
+    this.fetchCompany();
+  }
+
+  render() {
+    return this.state.company || "Unknown";
   }
 }
-const username = 'vsaravanan'
+const username = "vsaravanan";
 const element = (
-<div>
-<div>
-  {`@${username} works at `}
-  <UserCompany username={username} />
-</div>
-</div>
-)
+  <div>
+    <div>
+      {`@${username} works at `}
+      <UserCompany username={username} />
+    </div>
+  </div>
+);
 
 export default class App extends React.Component {
   render() {
-    return ( element) ;
+    return element;
   }
 }
