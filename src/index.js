a586cf4a9704ba88e70580af0c22d76bab18826f
V Saravanan <saravanan.resume@gmail.com>
2018-05-20 15:26:40 +0800

    map 1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from "react";
 
-function ListItem(props) {
-  return <li>{props.value}</li>;
-}
-
-function NumberList(props) {
-  const numbers = props.numbers;
-  return (
-    <ul>
-      {numbers.map((number) =>
-        <ListItem key={number.toString()}
-                  value={number} />
-      )}
-    </ul>
-  );
-}
 
 const numbers = [1, 2, 3, 4, 5];
 
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <ul>{listItems}</ul>,
   document.getElementById('root')
 );
