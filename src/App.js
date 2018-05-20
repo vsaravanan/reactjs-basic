0e2aa3b4182a449e3e05e705de1fe87572c8465e
V Saravanan <saravanan.resume@gmail.com>
2018-05-20 16:35:54 +0800

    prepare for redux

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
+export default class App extends Component {
+
+  state = {
+    todos: ['Click to remove', 'Learn React', 'Write Code', 'Ship App'],
+  }
+
+  onAddTodo = (text) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: [text, ...todos],
+    })
+  }
+
+  onRemoveTodo = (index) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: todos.filter((todo, i) => i !== index),
+    })
+  }
+
+  render() {
+    const {todos} = this.state
+
+    return (
+      <div style={styles.container}>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onClickItem={this.onRemoveTodo}
+        />
+      </div>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+}
