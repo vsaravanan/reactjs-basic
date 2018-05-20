4d3ae1b41a8a12f715b443fd577df6507650d60f
V Saravanan <saravanan.resume@gmail.com>
2018-05-20 15:34:02 +0800

    map 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import ReactDOM from 'react-dom';
 import React from "react";
 
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
   const numbers = props.numbers;
   const listItems = numbers.map((number) =>
-    <li>{number}</li>
+    // Correct! Key should be specified inside the array.
+    <ListItem key={number.toString()}
+              value={number} />
+
   );
   return (
-    <ul>{listItems}</ul>
+    <ul>
+      {listItems}
+    </ul>
   );
 }
 
